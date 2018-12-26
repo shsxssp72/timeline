0c108f0ba89fbf6039da7c90546d921b30ae3275
shsxssp72 <chenningyang1987@hotmail.com>
2018-12-27 00:06:02 +0800

    Test

---
+++ src/main/externalUtility/DataGenerator/cmake-build-debug/CMakeFiles/DataGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataGenerator.dir/main.cpp.obj"
+  "DataGenerator.pdb"
+  "DataGenerator.exe"
+  "DataGenerator.exe.manifest"
+  "libDataGenerator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
